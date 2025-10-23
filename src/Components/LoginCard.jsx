e823eea8e3079794f77f6ac34ba9e01de431cf69
Oleksandr Vasylovych <vasilovich.aleksandr@gmail.com>
2025-10-23 15:33:24 +0300

    CI-7 add navigate to login page

--- src/Components/LoginCard.jsx
+++ src/Components/LoginCard.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function LoginCard() {
+  const navigate = useNavigate();
+  
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -17,6 +21,7 @@ export default function LoginCard() {
       })
       .then((data) => {
         console.log("✅ Logged in:", data);
+        navigate("/main")
       })
       .catch((err) => {
         console.error("❌ Login error:", err);
